d0ebc27c52380de5d9b41b98e387c88d635eb1ff
joshua.hilljr198 <joshua.hilljr198@gmail.com>
2026-06-12 01:42:12 +0000

    save debug

--- pages/api/quotes/[id].js
+++ pages/api/quotes/[id].js
@@ -1,5 +1,5 @@
 import { doc, getDoc, updateDoc, deleteDoc } from 'firebase/firestore'
-import { db } from '../../../lib/firebase'
+import { db } from '../../lib/firebase'
 
 export default async function handler(req, res) {
   const { id } = req.query
